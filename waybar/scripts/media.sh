a95ad643298d49d3cc032ab04ac584f7e0667ba8
Bikram-ghuku <bikramghuku05@gmail.com>
2026-05-21 20:44:09 +0530

    feat: updates

--- waybar/scripts/media.sh
+++ waybar/scripts/media.sh
@@ -2,9 +2,12 @@
 
 # Max characters for display
 MAXLEN=30
+TMP_IMG="/tmp/hyde-mpris.png"
 
 # Get metadata
 metadata=$(playerctl metadata --format '{{ artist }} — {{ title }}' 2>/dev/null)
+arturl=$(playerctl metadata --format '{{ mpris:artUrl }}' 2>/dev/null)
+
 
 if [ -z "$metadata" ]; then
     text="No media"
@@ -20,6 +23,10 @@ fi
 # Get album for tooltip (optional)
 album=$(playerctl metadata --format '{{ album }}' 2>/dev/null)
 [ -z "$album" ] && album=""
+
+if [ -n "$arturl" ]; then
+    curl -s "$arturl" -o "$TMP_IMG"
+fi
 
 # Output JSON
 echo "{\"text\":\"$text\",\"icon\":\"󰎈\",\"tooltip\":\"$album\"}"
